78d33cc833f671b8f4fbc1428e8134ad8003b36b
Woosik Park <woosik.park@samsung.com>
2015-08-27 14:56:10 +0900

    change onshow event : setBannerstate/registerkey

--- cordova-js-src/platform.js
+++ cordova-js-src/platform.js
@@ -40,7 +40,14 @@ module.exports = {
             }
         }
 
-        channel.onNativeReady.fire();
+        var head = document.getElementsByTagName('head')[0];
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.src = '$MANAGER_WIDGET/Common/webapi/1.0/webapis.js';
+        script.onload = function() {
+            channel.onNativeReady.fire();
+        }
+        head.appendChild(script);
 
         window.onPause = function () {
             channel.onPause.fire();
@@ -50,6 +57,31 @@ module.exports = {
         };
         window.addEventListener('load', function () {
             window.onShow = function () {
+                var retval = null;
+                var body = document.getElementsByTagName('body')[0];
+                var objectElem = '<OBJECT id="SEFObject" classid="clsid:SAMSUNG-INFOLINK-SEF" style="display:block;position:absolute;width:0px;height:0px;"></OBJECT>';
+                body.innerHTML += objectElem;
+                var SEFObject = document.getElementById('SEFObject');
+                retval = SEFObject.Open('AppCommon','1.000','AppCommon');
+                console.log('SEF AppCommon Open.......'+retval);
+                //모든 키 해제 후 4-way navigation / enter / return key 만 등록
+                SEFObject.Execute('UnregisterAllKey');
+                SEFObject.Execute('RegisterKey',29460); //up
+                SEFObject.Execute('RegisterKey',29461); //down
+                SEFObject.Execute('RegisterKey',4); //left
+                SEFObject.Execute('RegisterKey',5); //right
+                SEFObject.Execute('RegisterKey',29443); //enter
+                SEFObject.Execute('RegisterKey',88); // return
+                retval = SEFObject.Close();
+                console.log('SEF AppCommon close.......'+retval);
+
+                retval = SEFObject.Open('NNavi','1.000','NNavi');
+                console.log('SEF NNavi Open.......'+retval);
+                // volume / channel banner 보이도록 설정.
+                SEFObject.Execute('SetBannerState',2);
+                retval = SEFObject.Close();
+                console.log('SEF NNavi close.......'+retval);
+
                 channel.onDeviceReady.fire();
             };
             if(window.curWidget && typeof curWidget.setPreference == 'function') {
