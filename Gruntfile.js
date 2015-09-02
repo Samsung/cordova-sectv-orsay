a4638afecefe42ee9436f7087b722f320928e237
Kookheon Kim <kookheon.kim@samsung.com>
2015-09-02 18:23:51 +0900

    applying jshint task

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      options: {
+          jshintrc: '.jshintrc',
+      },
+      src: ['cordova-js-src/**/*.js']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+};
