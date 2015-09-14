96ede08ec46352d24214d7a2226413cf1d2f36b9
Woosik Park <woosik.park@samsung.com>
2015-09-14 17:08:05 +0900

    Modify SEF.js

--- cordova-js-src/plugin/SEF.js
+++ cordova-js-src/plugin/SEF.js
@@ -1,56 +1,55 @@
-
 function SEF() {
-   this.PluginName = ['TV', 'TVMW', 'NNavi', 'Audio', 'AppCommon', 'FrontPanel', 'ImageViewer', 'Player', 'AUI',
+   this.pluginName = ['TV', 'TVMW', 'NNavi', 'Audio', 'AppCommon', 'FrontPanel', 'ImageViewer', 'Player', 'AUI',
                      'Storage', 'Network', 'Download', 'Screen', 'Time', 'Video', 'Window',
                      'ExternalWidgetInterface', 'FileSystem', 'Gamepad', 'Michrophone',
                      'CustomDevice', 'MIDIDevice','RECOG', 'AllShare'];
-   this.currentPlugin = null;
-   this.pluginSEF = "cordova_plugin_SEF";
+   
+   this.insertPluginList = {};
 }
 
-/**
- * Get SEF Plugin module
- *
- * @param {Function} successCallback The function to call when the heading data is available
- * @param {Function} errorCallback The function to call when there is an error getting the heading data. (OPTIONAL)
- */
+var pluginPrefix = 'cordova_plugin_SEF_';
 SEF.prototype.get = function(name) {
     if(__checkPluginName.apply(this,arguments)){
-        if(!document.getElementById(this.pluginSEF)) {
+        if(!this.insertPluginList.hasOwnProperty(name)) {
             var body = document.getElementsByTagName('body')[0];
-            body.innerHTML += '<OBJECT id="'+ this.pluginSEF + '" classid="clsid:SAMSUNG-INFOLINK-SEF" style="display:block;position:absolute;width:0px;height:0px;"></OBJECT>';
-        } else {
-            this.currentPlugin.Close();
-        }
+            body.innerHTML += '<OBJECT id="'+ pluginPrefix + name + '" classid="clsid:SAMSUNG-INFOLINK-SEF" style="display:block;position:absolute;width:0px;height:0px;"></OBJECT>';
+        } 
+        var currentPlugin = document.getElementById(pluginPrefix + name);
 
-        this.currentPlugin = document.getElementById(this.pluginSEF);
-        if(this.currentPlugin.Open(name,'1.000',name) === 1) {
-            return this.currentPlugin;
-        } else {
+        if(currentPlugin.Open(name,'1.000',name) === 1) {
+            this.insertPluginList.name = currentPlugin;
+            return currentPlugin;
+        } 
+        else {
             console.log('Failed to open SEF Plugin');
             throw Error('Failed to open SEF Plugin');
         }
-    } else {
+    } 
+    else {
         console.log('This SEF Plugin name is not supported');
         throw Error('This SEF Plugin name is not supported');
     }
 };
 
-SEF.prototype.close = function() {
-    
-    if(this.currentPlugin) {
-        this.currentPlugin.Close();
-    } else {
-        console.log('Failed to close SEF Plugin');
-        throw Error('Failed to close SEF Plugin');
+SEF.prototype.close = function(name) {
+    if(!name){
+        for(var key in this.insertPluginList){
+            this.insertPluginList[key].Close();
+            delete this.insertPluginList[key];
+        }
     }
+    else if(name && this.insertPluginList.hasOwnProperty(name)) {
+        this.insertPluginList.name.Close();
+        delete this.insertPluginList.name;
+    } 
+
 };
 
 function __checkPluginName(name){
-    for(var i=0; i<this.PluginName.length; i++) {
-        if(this.PluginName[i].toUpperCase() == name.toUpperCase()) {
-            console.log('\treturn '+this.PluginName[i]);
-            return this.PluginName[i];
+    for(var i=0; i<this.pluginName.length; i++) {
+        if(this.pluginName[i].toUpperCase() == name.toUpperCase()) {
+            console.log('\treturn '+this.pluginName[i]);
+            return this.pluginName[i];
         }
     }
     return null;
